96e4dccfe54456903796c4d5ea7372cea5babe91
Mr.Paint <50089388+PaintIsPainting@users.noreply.github.com>
2019-05-08 18:28:40 +0800

    Add files via upload

---
+++ MrPaintv2.sh
@@ -0,0 +1,544 @@
+#usr/bin/bash
+
+#Cuih.. Buka-Buka
+#Mau Recode Lu Anjing ?
+#Capek Gua Buat
+#Gua Bacain Yasin Lu
+#Biar Mampus x_x
+#Ijin Dulu Pekok
+#Chat Nomor Di Bawah
+clear
+
+lightgreen='\033[1;32m'
+i='\033[0;34'
+bi='\033[0;34m'
+okegreen='\033[92m'
+me='\033[1;31m'
+ku='\033[1;33m'
+pu='\033[1;37m'
+cyan='\033[0;36m'
+pur='\033[35;1m'
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $me"-.Sdh Nemu Yg D'cari?? "
+sleep 1
+echo $bi"-.Sorry Lw Kurang Lengkap"
+sleep 1
+echo $ku"-.Gua Hny Manusia Biasa"
+sleep 1
+echo $cyan"-.bye-bye:)"
+sleep 1
+clear
+exit
+}
+
+cd png
+python2 open.py
+cd
+echo
+echo
+echo
+echo $me" Siapa Namany Mastah?? "
+read name
+echo "Salam Kenal" $name
+echo ""
+echo $cyan"#"$me":"$cyan"#"$ku"...................................."$cyan"#"$me":"$cyan"#"
+echo $ku" :"$me"      Berjuang Demi Tanah Air"$ku"         :"
+echo $ku" :"$me"     Jangan Fikirkan Ego Doang"$ku"        :"
+echo $ku" :"$me"######################################"$ku":"
+echo $ku" :"$me" AUTHOR :"$cyan" 🖕ᢤ《Ϻr•ⱣⱥⲒﬣŧ》ᢤ🖕"$ku"        :"
+echo $ku" :"$me" Pesan  :"$ku" Mikir Sblum Brtindak"$ku"        :"
+echo $ku" :"$me" WA     :"$pu" 082293825257"$ku"                :"
+echo $ku" :"$me" Jenis  :"$me" Installer"$ku"                   :"
+echo $ku" :"$me" Rilis  :"$ku" Sabtu,4 Mei 2019"$ku"            :"
+echo $cyan"#"$me":"$cyan"#"$ku"...................................."$cyan"#"$me":"$cyan"#"
+echo
+echo $bi"##########################################"
+echo $bi"# Virus                                  #"
+echo $bi"##########################################"
+echo $bi"#["$me" 1"$bi" ]"$me" Malicious                         "$bi"#"
+echo $bi"#["$me" 2"$bi" ]"$me" Vbug                              "$bi"#"
+echo $bi"##########################################"
+echo $bi"# Spamming                               #"
+echo $bi"##########################################"
+echo $bi"#["$me" 3"$bi" ]"$me" Bom WhatsApp                      "$bi"#"
+echo $bi"#["$me" 4"$bi" ]"$me" UnliSpam                          "$bi"#"
+echo $bi"#["$me" 5"$bi" ]"$me" SpamSms1                          "$bi"#"
+echo $bi"#["$me" 6"$bi" ]"$me" JDID2                             "$bi"#"
+echo $bi"#["$me" 7"$bi" ]"$me" SpamSms (By:SeniTopeng)           "$bi"#"
+echo $bi"#["$me" 8"$bi" ]"$me" SpamCall                          "$bi"#"
+echo $bi"##########################################"
+echo $bi"# DDOS                                   #"
+echo $bi"##########################################"
+echo $bi"#["$me" 9"$bi" ]"$me" LITEDDOS                          "$bi"#"
+echo $bi"#["$me" 10"$bi"]"$me" DOS-TOOKITS                       "$bi"#"
+echo $bi"#["$me" 11"$bi"]"$me" Xerxes                            "$bi"#"
+echo $bi"#["$me" 12"$bi"]"$me" BLACK-CYBER-TROJAN                "$bi"#"
+echo $bi"#["$me" 13"$bi"]"$me" DARKDDOS                          "$bi"#"
+echo $bi"##########################################"
+echo $bi"# Script-Deface                          #"
+echo $bi"##########################################"
+echo $bi"#["$me" 14"$bi"]"$me" LITESCRIPT                        "$bi"#"
+echo $bi"#["$me" 15"$bi"]"$me" Ubai-Script-Creator               "$bi"#"
+echo $bi"#["$me" 16"$bi"]"$me" Create-Script-Deface              "$bi"#"
+echo $bi"##########################################"
+echo $bi"# Defacing                               #"
+echo $bi"##########################################"
+echo $bi"#["$me" 17"$bi"]"$me" AOXdeface                         "$bi"#"
+echo $bi"#["$me" 18"$bi"]"$me" AOCDEFACE                         "$bi"#"
+echo $bi"#["$me" 19"$bi"]"$me" Webdav77                          "$bi"#"
+echo $bi"##########################################"
+echo $bi"# Tambahan                               #"
+echo $bi"##########################################"
+echo $bi"#["$me" 20"$bi"]"$me" TOOLSINSTALLERv3                  "$bi"#"
+echo $bi"#["$me" 21"$bi"]"$me" viSQL                             "$bi"#"
+echo $bi"#["$me" 22"$bi"]"$me" Bingoo                            "$bi"#"
+echo $bi"#["$me" 23"$bi"]"$me" Ko-Dork                           "$bi"#"
+echo $bi"#["$me" 24"$bi"]"$me" ROBLOX-EXPLOIT-SCRIPTS            "$bi"#"
+echo $bi"#["$me" 25"$bi"]"$me" Santet-Online                     "$bi"#"
+echo $bi"#["$me" 26"$bi"]"$me" ASU.git                           "$bi"#"
+echo $bi"#["$me" 27"$bi"]"$me" BugHunter                         "$bi"#"
+echo $bi"#["$me" 28"$bi"]"$me" SQLMAP                            "$bi"#"
+echo $bi"#["$me" 29"$bi"]"$me" Mr.Paint                          "$bi"#"
+echo $bi"#["$me" 00"$bi"]"$me" Exit                              "$bi"#"
+echo $bi"##########################################"
+echo $me"•••"$bi"["$ku"Pilih Boss"$bi"]"$me"•••"
+read -p"~~~@" painting
+
+if [ $painting = 1 ] || [ $painting = 1 ]
+then
+clear
+figlet -f small -F gay "Installing Script" | lolcat
+echo $me" Loading "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/Hider5/Malicious
+cd Malicious
+chmod 777 Malicious.py
+apt install python2
+pip install --upgrade pip2
+pip2 install -r requirements.txt
+python2 malicious.py
+echo $ku" Saving Script "
+fi
+
+if [ $painting = 2 ] || [ $painting = 2 ]
+then
+clear
+figlet -f small -F gay "Install Tool" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/Junior60/vbug
+cd vbug
+chmod +x vbug.py
+python2 vbug.py
+echo $ku" Saving The Tool "
+fi
+
+if [ $painting = 3 ] || [ $painting = 3 ]
+then
+clear
+figlet -f small -F gay "Install Tool" | lolcat
+echo $me" From Us 2 U "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/siputra12/prank
+cd prank
+php wa.php
+echo $ku" Saving Tool "
+fi
+
+if [ $painting = 4 ] || [ $painting = 4 ]
+then
+clear
+figlet -f slant -F gay "Installing" | lolcat
+echo $me" From Us 2 U "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/Aditya021/UnliSpam
+cd UnliSpam
+pkg install vim
+vim call.py
+echo $ku" Tekan huruf ( i ) untuk mengedit"
+echo $ku"Cari api.php/nomor=xxxxxxx. Tanda xxxxxx ganti Menjadi No Target"
+echo $ku"Abistu Simpan Dengan Menekan Tombol ESC + :w (enter) lalu exit dengan menekan tombol ESC + :q (enter)"
+fi
+
+if [ $painting = 5 ] || [ $painting = 5 ]
+then
+clear
+figlet -f slant -F gay "Install Tool" | lolcat
+echo $me" From Us 2 U "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/KANG-NEWBIE/SpamSms
+cd SpamSms
+php sms.php
+echo $ku" Saving The Tool "
+fi
+
+if [ $painting = 6 ] || [ $painting = 6 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/Senitopeng/Jdid2
+cd Jdid2
+ls
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 7 ] || [ $painting = 7 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/Senitopeng/SpamSms.git
+cd SpamSms
+ls
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 8 ] || [ $painting = 8 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/Aditya021/SpamCall
+cd SpamCall
+php SpamCall.php
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 9 ] || [ $painting = 9 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+ls
+echo $ku" chmod Ndiri "
+fi
+
+if [ $painting = 10 ] || [ $painting = 10 ]
+then
+clear
+figlet -f slant -F gay " From Us 2 U " | lolcat
+echo $me" Install Tool "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/BPCATTACKER/DOS-TOOKITS
+cd DOS-TOOKITS
+sh DoS-TOOKIT.sh
+echo $ku" Saving Tool "
+fi
+
+if [ $painting = 11 ] || [ $painting = 11 ]
+then
+clear
+figlet -f slant -F gay " From Us 2 U " | lolcat
+echo $me" Install Tool "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/zanyarjamal/xerxes
+apt install clang
+cd xerxes
+clang xerxes.c -o xerxes
+clear
+read -p "[masukanWebsiteTarget]>" target
+./xerxes target 80
+fi
+
+if [ $painting = 12 ] || [ $painting = 12 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/BlackCyberTeam/BLACK-CYBER-TROJAN
+cd BLACK-CYBER-TROJAN
+sh BLACK-CYBER.sh
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 13 ] || [ $painting = 13 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/CyberTrojanIndonesia/DARKDDOS
+cd DARKDDOS
+sh DARKDDOS.sh
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 14 ] || [ $painting = 14 ]
+then
+clear
+figlet -f slant -F gay " From Us 2 U " | lolcat
+echo $me" Install Tool "
+sleep 1
+apt update
+apt upgrade
+git clone  https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+ls
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 15 ] || [ $painting = 15 ]
+then
+clear
+figlet -f slant -F gay " From Us 2 U " | lolcat
+echo $me" Install Tool "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/ubaii/script-deface-creator
+cd script-deface-creator
+chmod +x create.py
+python2 create.py
+echo $ku" Saving Tool "
+fi
+
+if [ $painting = 16 ] || [ $painting = 16 ]
+then
+clear
+figlet -f slant -F gay " From Us 2 U " | lolcat
+echo $me" Install Tool "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/Senitopeng/CreateScriptDeface.git
+cd CreateScriptDeface
+python2 script.py
+echo $ku" Saving The Tool "
+fi
+
+if [ $painting = 17 ] || [ $painting = 17 ]
+then
+clear
+figlet -f slant -F gay " From Us 2 U " | lolcat
+echo $me" Install Tool "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/Ranginang67/AOXdeface
+cd AOXdeface
+python2 aox.py
+echo $bi" Saving Tool "
+fi
+
+if [ $painting = 18 ] || [ $painting = 18 ]
+then
+clear
+figlet -f slant -F gay " From Us 2 U " | lolcat
+echo $me" Install Tool "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/Amriez/AOCDEFACE.git
+cd AOCDEFACE
+ls
+echo $bi" Saving Tool "
+fi
+
+if [ $painting = 19 ] || [ $painting = 19 ]
+then
+clear
+figlet -f slant -F gay " From Us 2 U " | lolcat
+echo $me" Install Tool "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/404rgr/webdav77
+cd webdav77
+bash webdav77.sh
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 20 ] || [ $painting = 20 ]
+then 
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+chmod +x TUANB4DUT.sh
+sh TUANB4DUT.sh
+echo $bi" Saving The Tools "
+fi
+
+if [ $painting = 21 ] || [ $painting = 21 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/blackvkng/viSQL
+cd viSQL
+ls
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 22 ] || [ $painting = 22 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+apt install grep
+apt install linx
+git clone https://github.com/Hood3dRob1n/BinGoo.git
+cd BinGoo
+termux-fix-shebang bingoo
+chmod +x bingoo
+bash bingoo
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 23 ] || [ $painting = 23 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/ciku370/ko-dork
+cd ko-dork
+python2 dork.py
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 24 ] || [ $painting = 24 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/OfficerBamford/ROBLOX-EXPLOIT-SCRIPTS.git
+cd ROBLOX-EXPLOIT-SCRIPTS
+ls
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 25 ] || [ $painting = 25 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python2 santet.py
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 26 ] || [ $painting = 26 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/LOoLzeC/ASU.git
+cd ASU
+ls
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 27 ] || [ $painting = 27 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/thehackingsage/bughunter
+cd bughunter
+python2 bughunter.py
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 28 ] || [ $painting = 28 ]
+then
+clear
+figlet -f slant -F gay "From Us 2 U" | lolcat
+echo $me" LOADING "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+ls
+echo $bi" Saving The Tool "
+fi
+
+if [ $painting = 29 ] || [ $painting = 29 ]
+then
+clear
+figlet -f slant -F gay " From Us 2 U " | lolcat
+echo $me" Install Tool "
+sleep 1
+apt update
+apt upgrade
+git clone https://github.com/PaintIsPainting/Mr.Paint.git
+cd Mr.Paint
+sh Mr.Paint.sh
+echo $me" Saving The Tools "
+fi
+
+if [ $painting = 00 ] || [ $painting = 00 ]
+then
+clear
+figlet -f slant -F gay " Wanna GO ?? " | lolcat
+echo $me" And That's Enought "
+echo "bye-bye :)"
+clear
+exit
+fi
